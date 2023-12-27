812c728a3b129d009f5b5eb7d8f28d640c6ff1ed
codewithkaranjeswani <krnjeswani21@gmail.com>
2023-12-27 23:35:51 +0530

    first page

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,9 @@
-import { type Config } from "drizzle-kit";
-
 import { env } from "@/env";
+import { type Config } from "drizzle-kit";
 
 export default {
   schema: "./src/server/db/schema.ts",
+  out: "./src/server/db/migrations",
   driver: "mysql2",
   dbCredentials: {
     uri: env.DATABASE_URL,
