97145d04ed9a411f06ea037327eed140955fdbcc
codewithkaranjeswani <krnjeswani21@gmail.com>
2023-12-29 02:05:58 +0530

    db schema upd

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -21,53 +21,92 @@ import {
  */
 export const mysqlTable = mysqlTableCreator((name) => `dl_${name}`);
 
-export const lists = mysqlTable(
-  "list",
+export const courses = mysqlTable(
+  "course",
   {
     id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    title: varchar("title", { length: 255 }),
+    title: bigint("title", { mode: "number" }).notNull(),
+    description: varchar("description", { length: 255 }),
+    cost: bigint("cost", { mode: "number" }),
     authorId: varchar("author_id", { length: 255 }).notNull(),
     createdAt: timestamp("created_at").notNull(),
     updatedAt: timestamp("updated_at").notNull(),
   },
-  (list) => ({
-    authorIdIdx: index("author_id_idx").on(list.authorId),
+  (course) => ({
+    authorIdIdx: index("author_id_idx").on(course.authorId),
+    descriptionIndex: index("description_idx").on(course.description),
   }),
 );
 
-export const listRelations = relations(lists, ({ one, many }) => ({
-  todos: many(todos),
+export const courseRelations = relations(courses, ({ one, many }) => ({
+  quizes: many(quizes),
+  usersToCourses: many(usersToCourses),
   author: one(users, {
-    fields: [lists.authorId],
+    fields: [courses.authorId],
     references: [users.id],
   }),
 }));
 
-export const todos = mysqlTable(
-  "todo",
-  {
-    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    listId: bigint("list_id", { mode: "number" }).notNull(),
-    content: varchar("content", { length: 255 }),
-    completed: boolean("completed").default(false).notNull(),
-    authorId: varchar("author_id", { length: 255 }).notNull(),
-    createdAt: timestamp("created_at").notNull(),
-    updatedAt: timestamp("updated_at").notNull(),
-  },
-  (todo) => ({
-    authorIdIdx: index("author_id_idx").on(todo.authorId),
-    contentIndex: index("content_idx").on(todo.content),
-  }),
-);
+export const quizes = mysqlTable("quiz", {
+  id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
+  title: bigint("title", { mode: "number" }).notNull(),
+  description: varchar("description", { length: 255 }),
+  authorId: varchar("author_id", { length: 255 }).notNull(),
+  courseId: bigint("course_id", { mode: "number" }).notNull(),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+});
 
-export const todoRelations = relations(todos, ({ one }) => ({
-  lists: one(lists, {
-    fields: [todos.listId],
-    references: [lists.id],
+export const quizesRelations = relations(quizes, ({ one, many }) => ({
+  questions: many(questions),
+  course: one(courses, {
+    fields: [quizes.courseId],
+    references: [courses.id],
   }),
   author: one(users, {
-    fields: [todos.authorId],
+    fields: [quizes.authorId],
     references: [users.id],
+  }),
+}));
+
+export const questions = mysqlTable("question", {
+  id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
+  qtext: varchar("qtext", { length: 255 }).notNull(),
+  atext: varchar("atext", { length: 255 }).notNull(),
+  order: int("order").notNull(),
+  description: varchar("description", { length: 255 }),
+  authorId: varchar("author_id", { length: 255 }).notNull(),
+  quizId: bigint("quiz_id", { mode: "number" }).notNull(),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+});
+
+export const questionsRelations = relations(questions, ({ one }) => ({
+  author: one(users, {
+    fields: [questions.authorId],
+    references: [users.id],
+  }),
+  quiz: one(quizes, {
+    fields: [questions.quizId],
+    references: [quizes.id],
+  }),
+}));
+
+export const answers = mysqlTable("answer", {
+  id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
+  content: varchar("content", { length: 255 }).notNull(),
+  correct: boolean("correct").default(false).notNull(),
+  description: varchar("description", { length: 255 }),
+  authorId: varchar("author_id", { length: 255 }).notNull(),
+  questionId: bigint("question_id", { mode: "number" }).notNull(),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+});
+
+export const answersRelations = relations(answers, ({ one }) => ({
+  question: one(questions, {
+    fields: [answers.questionId],
+    references: [questions.id],
   }),
 }));
 
@@ -80,13 +119,29 @@ export const users = mysqlTable("user", {
     fsp: 3,
   }).default(sql`CURRENT_TIMESTAMP(3)`),
   image: varchar("image", { length: 255 }),
-  role: varchar("role", { length: 255, enum: ["User", "Admin"] }),
+  role: varchar("role", { length: 255, enum: ["User", "Educator", "Admin"] }),
   password: varchar("password", { length: 255 }),
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
-  todos: many(todos),
-  tlists: many(lists),
+  usersToCourses: many(usersToCourses),
+}));
+
+export const usersToCourses = mysqlTable("users_to_courses", {
+  id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
+  userId: bigint("user_id", { mode: "number" }),
+  courseId: bigint("course_id", { mode: "number" }),
+});
+
+export const usersToCoursesRelations = relations(usersToCourses, ({ one }) => ({
+  course: one(courses, {
+    fields: [usersToCourses.courseId],
+    references: [courses.id],
+  }),
+  user: one(users, {
+    fields: [usersToCourses.userId],
+    references: [users.id],
+  }),
 }));
 
 export const accounts = mysqlTable(
