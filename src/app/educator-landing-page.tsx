591b18d23a0e9624960932715dca153f3bd938f7
codewithkaranjeswani <krnjeswani21@gmail.com>
2023-12-30 03:16:02 +0530

    using lenient eslint config to succeed in build

--- src/app/educator-landing-page.tsx
+++ src/app/educator-landing-page.tsx
@@ -2,6 +2,9 @@
 import { api } from "@/trpc/react";
 import CreateCourseForm from "./create-course-form";
 
+// TODO:
+// make the courses clickable to go to course page
+// each course page should have a form to create quizes
 export default function EducatorLandingPage() {
   const { data: courses } = api.course.getMine.useQuery();
 
@@ -14,10 +17,11 @@ export default function EducatorLandingPage() {
       </div>
       <CreateCourseForm />
       <div className="font-semibold">
-        Here are the list of courses you have created
+        Here are the list of courses you have created. Only the latest will be
+        shown to learners.
       </div>
-      <div className="py-5" />
-      <div>
+      <div className="border-b py-5" />
+      <div className="border-b">
         {courses?.map((course) => (
           <div key={`one-${course.id}`}>
             <div key={`two-${course.id}`}>{course.title}</div>
